4bcb3c6f5e71ba0a063da08f8a973f6c18258d90
秦兴汉 <sean@qinxinghandeMacBook-Pro.local>
2019-12-06 12:26:55 -0500

    finish

--- src/searchengine/GetWebText.java
+++ src/searchengine/GetWebText.java
@@ -11,9 +11,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
